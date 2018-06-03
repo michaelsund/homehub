e22b668afecc61d78dbdf0837f6e908e70bfd07c
Michael Sund <michael@osund.com>
2018-06-03 21:01:15 +0200

    added tooltip example to work from

--- client/src/components/LineChart/LineChart.js
+++ client/src/components/LineChart/LineChart.js
@@ -1,10 +1,16 @@
 // @flow
 
 import React from 'react'
-import { letterFrequency } from '@vx/mock-data'
-import { Group } from '@vx/group'
-import { Bar } from '@vx/shape'
-import { scaleLinear, scaleBand } from '@vx/scale'
+import { AreaClosed, Line, Bar } from '@vx/shape'
+import { appleStock } from '@vx/mock-data'
+import { curveMonotoneX } from '@vx/curve'
+import { LinearGradient } from '@vx/gradient'
+import { GridRows, GridColumns } from '@vx/grid'
+import { scaleTime, scaleLinear } from '@vx/scale'
+import { withTooltip, Tooltip } from '@vx/tooltip'
+import { localPoint } from '@vx/event'
+import { extent, max, bisector } from 'd3-array'
+import { timeFormat } from 'd3-time-format'
 
 type Props = {
   width: number,
@@ -12,47 +18,219 @@ type Props = {
   margin: Object
 }
 
+const stock = appleStock.slice(800)
+const formatDate = timeFormat("%b %d, '%y")
+
+// accessors
+const xStock = d => new Date(d.date)
+const yStock = d => d.close
+const bisectDate = bisector(d => new Date(d.date)).left
+
+
 class LineChart extends React.Component<Props> {
+  constructor(props) {
+    super(props)
+    this.handleTooltip = this.handleTooltip.bind(this)
+  }
+
+  handleTooltip = ({
+    event, data, xStock, xScale, yScale
+  }) => {
+    const { showTooltip } = this.props
+    const { x } = localPoint(event)
+    const x0 = xScale.invert(x)
+    const index = bisectDate(data, x0, 1)
+    const d0 = data[index - 1]
+    const d1 = data[index]
+    let d = d0
+    if (d1 && d1.date) {
+      d = x0 - xStock(d0.date) > xStock(d1.date) - x0 ? d1 : d0
+    }
+    showTooltip({
+      tooltipData: d,
+      tooltipLeft: x,
+      tooltipTop: yScale(d.close),
+    })
+  }
+
   render() {
-    const data = letterFrequency
-    const xMax = this.props.width - this.props.margin.left - this.props.margin.right
-    const yMax = this.props.height - this.props.margin.top - this.props.margin.bottom
-    const x = d => d.letter
-    const y = d => +d.frequency * 100
-    const xScale = scaleBand({
-      rangeRound: [0, xMax],
-      domain: data.map(x),
-      padding: 0.4,
-    })
+    const {
+      width,
+      height,
+      margin,
+      showTooltip,
+      hideTooltip,
+      tooltipData,
+      tooltipTop,
+      tooltipLeft,
+      events,
+    } = this.props;
+    if (width < 10) return null;
 
+    // bounds
+    const xMax = this.props.width - this.props.margin.left - this.props.margin.right;
+    const yMax = this.props.height - this.props.margin.top - this.props.margin.bottom;
+
+    // scales
+    const xScale = scaleTime({
+      range: [0, xMax],
+      domain: extent(stock, xStock),
+    });
     const yScale = scaleLinear({
-      rangeRound: [yMax, 0],
-      domain: [0, Math.max(...data.map(y))],
-    })
-
-    const compose = (scale, accessor) => data => scale(accessor(data))
-    const xPoint = compose(xScale, x)
-    const yPoint = compose(yScale, y)
+      range: [yMax, 0],
+      domain: [0, max(stock, yStock) + yMax / 3],
+      nice: true,
+    });
 
     return (
-      <svg width={this.props.width} height={this.props.height}>
-        {data.map((d, i) => {
-          const barHeight = yMax - yPoint(d)
-          return (
-            <Group key={`bar-${i}`}>
-              <Bar
-                x={xPoint(d)}
-                y={yMax - barHeight}
-                height={barHeight}
-                width={xScale.bandwidth()}
-                fill='#fc2e1c'
+      <div>
+        <svg ref={s => (this.svg = s)} width={this.props.width} height={this.props.height}>
+          <rect
+            x={0}
+            y={0}
+            width={this.props.width}
+            height={this.props.height}
+            fill="#32deaa"
+            rx={14}
+          />
+          <defs>
+            <linearGradient
+              id="gradient"
+              x1="0%"
+              y1="0%"
+              x2="0%"
+              y2="100%"
+            >
+              <stop
+                offset="0%"
+                stopColor="#FFFFFF"
+                stopOpacity={1}
               />
-            </Group>
-          )
-        })}
-      </svg>
-    )
+              <stop
+                offset="100%"
+                stopColor="#FFFFFF"
+                stopOpacity={0.2}
+              />
+            </linearGradient>
+          </defs>
+          <GridRows
+            lineStyle={{ pointerEvents: 'none' }}
+            scale={yScale}
+            width={xMax}
+            strokeDasharray="2,2"
+            stroke="rgba(255,255,255,0.3)"
+          />
+          <GridColumns
+            lineStyle={{ pointerEvents: 'none' }}
+            scale={xScale}
+            height={yMax}
+            strokeDasharray="2,2"
+            stroke="rgba(255,255,255,0.3)"
+          />
+          <AreaClosed
+            data={stock}
+            xScale={xScale}
+            yScale={yScale}
+            x={xStock}
+            y={yStock}
+            strokeWidth={1}
+            stroke={'url(#gradient)'}
+            fill={'url(#gradient)'}
+            curve={curveMonotoneX}
+          />
+          <Bar
+            x={0}
+            y={0}
+            width={width}
+            height={height}
+            fill="transparent"
+            rx={14}
+            data={stock}
+            onTouchStart={data => event =>
+              this.handleTooltip({
+                event,
+                data,
+                xStock,
+                xScale,
+                yScale,
+              })}
+            onTouchMove={data => event =>
+              this.handleTooltip({
+                event,
+                data,
+                xStock,
+                xScale,
+                yScale,
+              })}
+            onMouseMove={data => event =>
+              this.handleTooltip({
+                event,
+                data,
+                xStock,
+                xScale,
+                yScale,
+              })}
+            onMouseLeave={data => event => hideTooltip()}
+          />
+          {tooltipData && (
+            <g>
+              <Line
+                from={{ x: tooltipLeft, y: 0 }}
+                to={{ x: tooltipLeft, y: yMax }}
+                stroke="rgba(92, 119, 235, 1.000)"
+                strokeWidth={2}
+                style={{ pointerEvents: 'none' }}
+                strokeDasharray="2,2"
+              />
+              <circle
+                cx={tooltipLeft}
+                cy={tooltipTop + 1}
+                r={4}
+                fill="black"
+                fillOpacity={0.1}
+                stroke="black"
+                strokeOpacity={0.1}
+                strokeWidth={2}
+                style={{ pointerEvents: 'none' }}
+              />
+              <circle
+                cx={tooltipLeft}
+                cy={tooltipTop}
+                r={4}
+                fill="rgba(92, 119, 235, 1.000)"
+                stroke="white"
+                strokeWidth={2}
+                style={{ pointerEvents: 'none' }}
+              />
+            </g>
+          )}
+        </svg>
+        {tooltipData && (
+          <div>
+            <Tooltip
+              top={tooltipTop - 12}
+              left={tooltipLeft + 12}
+              style={{
+                backgroundColor: 'rgba(92, 119, 235, 1.000)',
+                color: 'white',
+              }}
+            >
+              {`$${yStock(tooltipData)}`}
+            </Tooltip>
+            <Tooltip
+              top={yMax - 14}
+              left={tooltipLeft}
+              style={{
+                transform: 'translateX(-50%)',
+              }}
+            >
+              {formatDate(xStock(tooltipData))}
+            </Tooltip>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
-export default LineChart
+export default withTooltip(LineChart)
