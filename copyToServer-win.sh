3449de505662246813322e3f3b0bf36df0f39600
Michael <michael@osund.com>
2018-06-06 14:19:44 +0200

    copy script for win

---
+++ copyToServer-win.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Tars and copies itself to server
+#
+echo "DONT FORGET TO SET DEV TO FALSE!!!!"
+sleep 5
+rm -r node_modules
+rm -r client/node_modules
+rm ..\hut.tar.gz
+cd .. && tar pczf hut.tar.gz hut && scp hut.tar.gz michael@192.168.1.2:~/
+ssh michael@192.168.1.2 "rm Dockerbuilds/hut.tar.gz && mv hut.tar.gz Dockerbuilds/ && cd Dockerbuilds && rm -r hut && tar zxf hut.tar.gz && cd hut && docker build -t hut ."
+echo "Done with Server! reinstalling local node modules..."
+cd hut && yarn installdeps
