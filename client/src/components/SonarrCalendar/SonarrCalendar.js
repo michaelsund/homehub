4d66226cfbbfbee27fa6414c783ecb1bbdcb3d83
Michael Sund <michael@osund.com>
2018-07-03 17:14:39 +0200

    Sonarr list styling

--- client/src/components/SonarrCalendar/SonarrCalendar.js
+++ client/src/components/SonarrCalendar/SonarrCalendar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import settings from '../../settings.json'
+import './SonarrCalendar.css'
 
 type State = {
   series: Array
@@ -18,13 +19,26 @@ class SonarrCalendar extends React.Component<{}, State> {
       .then(series => this.setState({ series }))
   }
 
+  hasFile = downloaded => downloaded ? (
+    <span style={{ fontSize: '10px', color: '#18D8F0' }}> Downloaded</span>
+  ) : (
+    <span style={{ fontSize: '10px', color: '#FF1493' }}> Waiting</span>
+  )
+
   render() {
     return (
-      <div className="col-wrapper">
-        <ul>
-          {this.state.series.map(serie => <li key={serie.id}>
-            {serie.hasFile} {serie.airDate} - {serie.series.title} - {serie.overview}
-          </li>)}
+      <div className="col-wrapper sonarr-container">
+        <ul className="list-dot_hidden sonarr-list">
+           {this.state.series.map(serie => <li className="sonarr-serieitem" key={serie.id}>
+              <p className="sonarr-seriename">{serie.series.title}</p>
+              <div className="sonarr-serieinfo">
+                <span style={{ fontSize: '10px', color: 'rgba(255, 255, 255, .25)' }}>
+                  {serie.airDate} S{serie.seasonNumber} E{serie.episodeNumber}
+                </span>
+                <span>{this.hasFile(serie.hasFile)}</span>
+                {/* {JSON.stringify(serie.hasFile)} {serie.airDate} - {serie.series.title} */}
+              </div>
+           </li>)}
         </ul>
       </div>
     )
