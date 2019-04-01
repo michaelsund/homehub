8a454e23d9d46643db58725aa4d2ed6b95d669c8
Michael Sund <michael@osund.com>
2019-04-01 19:47:35 +0200

    updated threshold for sensor alarms

--- server/helpers/checkSensorAlarmTrigger.js
+++ server/helpers/checkSensorAlarmTrigger.js
@@ -2,17 +2,42 @@ import db from '../schema'
 import sensorsPubSub from './sensorsPubSub'
 import Send from './emitPushbulletMessage'
 
+// Local array keeping tabs on triggertimes, for sensors that
+// sometimes trigger a false positive.
+const triggerCountThreshold = 10
+const sensorsTriggerTimes = []
+
 const checkSensorAlarmTrigger = (sensor, currentValue) => {
   if (sensor.maxValueAlarm) {
     if (currentValue > sensor.maxValue && !sensor.maxValueAlarmActive) {
-      db.Sensor.findOne({ _id: sensor._id }, (err, sensor) => {
-        sensor.update({ maxValueAlarmActive: true }, () => {
-          sensorsPubSub()
-          // Avoid spamming, just send if there is no alarm active.
-          if (!sensor.maxValueAlarmActive) {
-            Send(sensor.name, `exceeded max value: ${sensor.maxValue} with ${currentValue}`)
+      if (!sensorsTriggerTimes.find(s => s._id === sensor._id)) {
+        sensorsTriggerTimes.push({ _id: sensor._id, triggerTimes: 1 })
+      } else {
+        sensorsTriggerTimes.map(s => {
+          if (s._id === sensor._id) {
+            s.triggerTimes += 1;
+            if (s.triggerTimes >= triggerCountThreshold) {
+              // Reset triggertimes and set sensor to alarm.
+              s.triggerTimes = 0
+              db.Sensor.findOne({ _id: sensor._id }, (err, sensor) => {
+                sensor.update({ maxValueAlarmActive: true }, () => {
+                  sensorsPubSub()
+                  // Avoid spamming, just send if there is no alarm active.
+                  if (!sensor.maxValueAlarmActive) {
+                    Send(sensor.name, `exceeded max value: ${sensor.maxValue} with ${currentValue}`)
+                  }
+                })
+              })
+            }
           }
         })
+      }
+    } else {
+      sensorsTriggerTimes.map(s => {
+        // Reset triggercount
+        if (s._id === sensor._id) {
+          s.triggerTimes = 0
+        }
       })
     }
   }
